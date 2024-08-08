e36a25617b673ef28eaa5e4826a0f170b25969ab
anacamorims <ana.camorims.dev@gmail.com>
2024-08-08 17:52:48 -0400

    🗃️ raw:add folder connection db

---
+++ src/database/connection.db.js
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize";
+
+export const database = new Sequelize("postgresql://postgres.ddleqszawxexidqdwxfa:r1L9usYvIX4lGUQs@aws-0-us-east-1.pooler.supabase.com:6543/postgres", {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,
+});
+
+
+database.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
