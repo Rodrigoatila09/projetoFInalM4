6cc7377e82506042cb0a9566b7c36c469b556ff3
anacamorims <ana.camorims.dev@gmail.com>
2024-08-09 14:43:52 -0400

    ✨ feat: add route delete

--- src/routes/whistleblower.routes.js
+++ src/routes/whistleblower.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { createWhistleblower, getWhistleblower, updateWhistleblower } from "../controllers/whistleblower.controller.js";
+import { createWhistleblower, getWhistleblower, updateWhistleblower, deleteWhistleblower } from "../controllers/whistleblower.controller.js";
 
 const whistleblowerRouter = Router();
 
 whistleblowerRouter.post('/whistleblower', createWhistleblower);
 whistleblowerRouter.get('/whistleblower', getWhistleblower);
 whistleblowerRouter.put('/whistleblower/:id', updateWhistleblower);
+whistleblowerRouter.delete('/whistleblower/:id', deleteWhistleblower);
+
 
 
 
