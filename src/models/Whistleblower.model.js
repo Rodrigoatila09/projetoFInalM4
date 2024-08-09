0e9b70a1792b88ee2d443569057b87b1abd9bfc3
anacamorims <ana.camorims.dev@gmail.com>
2024-08-09 14:42:50 -0400

    🐛 fix: model whisteblower

--- src/models/Whistleblower.model.js
+++ src/models/Whistleblower.model.js
@@ -25,11 +25,11 @@ export const Whistleblower = database.define("Whistleblower", {
   },
 });
 
-
-database.sync()
+database
+  .sync()
   .then(() => {
-    console.log('Database synchronized successfully.');
+    console.log("Database synchronized successfully.");
   })
-  .catch(err => {
-    console.error('Error synchronizing the database:', err);
+  .catch((err) => {
+    console.error("Error synchronizing the database:", err);
   });
