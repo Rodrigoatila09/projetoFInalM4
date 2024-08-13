4102997da5117e45de7c93491f9cc0c9a26ce059
anacamorims <ana.camorims.dev@gmail.com>
2024-08-13 19:53:43 -0400

    ✨ feat:calling functions

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import { whistleblowerRouter } from "./routes/whistleblower.routes.js"
 import { evidenceRouter } from "./routes/evidence.routes.js"
 import { responsibleAuthorityRouter } from "./routes/responsibleAuthority.routes.js"
 import { crimeSceneRouter } from "./routes/crimeScene.routes.js"
-
+import { resolutionRouter } from "./routes/resolution.routes.js"
 
 const app = express()
 const port = 4000
@@ -11,13 +11,10 @@ const port = 4000
 app.use(express.json())
 
 app.use(whistleblowerRouter)
-
-
 app.use(evidenceRouter)
-
 app.use(responsibleAuthorityRouter)
-
 app.use(crimeSceneRouter)
+app.use(resolutionRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
