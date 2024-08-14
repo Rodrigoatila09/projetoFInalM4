4314ac4ef9c5099af99a217a1033807db63a3023
anacamorims <ana.camorims.dev@gmail.com>
2024-08-14 18:58:36 -0400

    🐛 fix: fixing script db in server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 import express from "express"
 import cors from "cors"
+import { connection } from "./database/connection.db.js"
 import { whistleblowerRouter } from "./routes/whistleblower.routes.js"
 import { evidenceRouter } from "./routes/evidence.routes.js"
 import { responsibleAuthorityRouter } from "./routes/responsibleAuthority.routes.js"
 import { resolutionRouter } from "./routes/resolution.routes.js"
-import { complaimantRouter } from "./routes/complaint.routes.js"
+import { complaintRouter } from "./routes/complaint.routes.js"
 import { crimeSceneRouter } from "./routes/crimeScene.routes.js"
 import { categoryReportRouter } from "./routes/categoryReport.routes.js"
-import { database } from "./database/connection.db.js";
-import "./database/sync.js"
+
 
 const app = express()
 const port = 4000
@@ -23,15 +23,14 @@ app.use(cors(corsConfig))
 app.use(express.json())
 
 app.use(responsibleAuthorityRouter)
-
-app.use(complaimantRouter)
+app.use(complaintRouter)
 app.use(whistleblowerRouter)
 app.use(evidenceRouter)
 app.use(resolutionRouter)
 app.use(crimeSceneRouter)
 app.use(categoryReportRouter)
-app.use(responsibleAuthorityRouter)
 
 app.listen(port, () => {
+  connection()
   console.log(`Server is running on port ${port}`);
 });
